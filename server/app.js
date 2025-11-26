961f0fe73eb3ba111316379754e277e17a63af7e
Chandler Duffey <wwp24@txstate.edu>
2025-11-25 21:28:40 -0600

    fix import issues, server runs, query endpoint live

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
+
+const { searchFragrancesByNotesAll } = require('./routes/searchFragrancesByNotesAll');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('/api/fragrances/search', searchFragrancesByNotesAll);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
---
+++ server/routes/searchFragrancesByNotesAll.js
@@ -0,0 +1,46 @@
+// import { prisma } from '../utils/prisma';
+const { prisma } = require('../utils/prisma.ts');
+
+
+const searchFragrancesByNotesAll = async (req, res) => {
+    const notesString = req.query.notes;
+
+    const requiredNotes = notesString.toLowerCase().split(',').map(note => note.trim()).filter(Boolean);
+
+    try {
+        const andConditions = requiredNotes.map(noteName => ({
+            fragrance_notes: {
+                some: {
+                    scent_terms: {
+                        term: {
+                            equals: noteName,
+                            mode: 'insensitive',
+                        },
+                    },
+                },
+            },
+        }));
+
+        const fragrances = await prisma.fragrances.findMany({
+            where: {
+                AND: andConditions,
+            },
+            include: {
+                fragrance_notes: {
+                    include: {
+                        scent_terms: true,
+                    },
+                },
+            },
+        });
+
+        res.json(fragrances);
+    } catch (error) {
+        console.error('Error during fragrance search:', error);
+        res.status(500).json({ error: 'An error occurred.' });
+    }
+};
+
+module.exports = {
+    searchFragrancesByNotesAll,
+};
